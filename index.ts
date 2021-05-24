c81aa74f76b2b192a8b36a52441e82eb4ff1a591
Clement Okyere <clement.okyere18@gmail.com>
2021-05-24 01:16:40 +0000

    added express

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("hello AwesomeTodo API!");
+import express from "express";
+const app = express();
+
+const PORT = process.env.port || 3000;
+app.listen(PORT, () => {
+    console.log(`Awesome Todo Api listening on ${PORT}!`);
+});
+
