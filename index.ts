ff98b70a3613e2363bddfb6b95c819e4c398b7cc
Clement Okyere <clement.okyere18@gmail.com>
2021-05-24 03:23:17 +0000

    implemented mongoose db connection

--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import route from "./routes/todo";
+import routes from "./startup/routes";
 const app = express();
+
+mongoose
+    .connect("mongodb://localhost/todo", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+  .then(() => console.log("connected to mongodb successfully"))
+  .catch((err) => console.log("error connecting to mongodb"));
+  
+
+routes(app);
 
 const PORT = process.env.port || 3000;
 app.listen(PORT, () => {
