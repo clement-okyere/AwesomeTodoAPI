bb1e5a81a2d1c358b5ebfcb2fc1e28c5d02b2e87
Clement Okyere <clement.okyere18@gmail.com>
2021-06-27 12:25:45 +0000

    added auth route

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -1,4 +1,5 @@
 import todos from '../routes/todo';
+import auth from '../routes/auth';
 import express from "express";
 import cors from "cors";
 
@@ -8,6 +9,7 @@ const routes = (app: express.Application) => {
    }));
    app.use(express.json());
    app.use("/api/todos", todos);
+   app.use("/api/auth", auth);
 }
 
 export default routes;
